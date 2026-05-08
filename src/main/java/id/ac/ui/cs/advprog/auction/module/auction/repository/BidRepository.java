4c9ceaa3d805f3a3704052488b0826616acd44de
Rainmo0 <aunknown145@gmail.com>
2026-05-08 17:17:39 +0700

    make a function to count many bid in one auction (one listing)

--- src/main/java/id/ac/ui/cs/advprog/auction/module/auction/repository/BidRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/auction/module/auction/repository/BidRepository.java
@@ -3,10 +3,17 @@ package id.ac.ui.cs.advprog.auction.module.auction.repository;
 import id.ac.ui.cs.advprog.auction.module.auction.model.Bid;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BidRepository extends JpaRepository<Bid, Long> {
     List<Bid> findByAuctionIdOrderByCreatedAtDesc(Long auctionId);
 
     Optional<Bid> findTopByAuctionIdOrderByAmountDescCreatedAtAsc(Long auctionId);
+
+    @Query("select count(b) from Bid b where b.auction.listingId = :listingId")
+    long countByListingId(@Param("listingId") UUID listingId);
 }
